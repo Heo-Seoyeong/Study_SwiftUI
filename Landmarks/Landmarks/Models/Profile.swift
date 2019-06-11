6865b9c7199c7d20cf51d1d88bce9a0e80daecfc
Seoyeong <gjtjdud93@gmail.com>
2019-06-11 14:48:06 +0900

    Working with UI Controls

---
+++ Landmarks/Landmarks/Models/Profile.swift
@@ -0,0 +1,33 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Seoyeong Heo on 2019/06/11.
+//  Copyright © 2019 landmarks. All rights reserved.
+//
+
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications: Bool
+    var seasonalPhoto: Season
+    var goalDate: Date
+    
+    static let `default` = Self(username: "g_kumar", prefersNotifications: true, seasonalPhoto: .winter)
+    
+    init(username: String, prefersNotifications: Bool = true, seasonalPhoto: Season = .winter) {
+        self.username = username
+        self.prefersNotifications = prefersNotifications
+        self.seasonalPhoto = seasonalPhoto
+        self.goalDate = Date()
+    }
+    
+    enum Season: String, CaseIterable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+    }
+}
